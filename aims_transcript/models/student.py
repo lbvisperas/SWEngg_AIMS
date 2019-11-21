74869680b087cb28925b45a760afacdec731079c
lananh03 <57034788+lananh03@users.noreply.github.com>
2019-11-22 00:45:50 +0700

    Delete student.py

--- aims_transcript/models/student.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-
-class StudentStudent(models.Model):
-    _inherit = "student.student"
-    student_transcript = fields.One2many('student.transcript', 'student_id', string='Student_Transcript')
