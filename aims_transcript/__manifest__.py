130952c51be79c5222e4fba4768bc6436214efee
lananh03 <57034788+lananh03@users.noreply.github.com>
2019-11-22 00:51:13 +0700

    Add files via upload

--- aims_transcript/__manifest__.py
+++ aims_transcript/__manifest__.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Transcript",
-
-    'summary': "Record of Student Transcript Information",
-    'category': "Education and Employment",
-    'version': "1.0",
-    'author': "Tran Lan Anh",
-    'maintainer': "Tran Lan Anh",
-    'company': "Lan",
-    'website': "https://www.tggs.kmutnb.ac.th",
-
+    'name': 'Transcript',
+    'version': '1.0',
+    'summary': 'Record of Student Transcript Information',
+    'category': 'Education and Employment',
+    'author': 'Tran Lan Anh',
+    'maintainer': 'Tran Lan Anh',
+    'company': 'Lan',
+    'website': 'https://www.tggs.kmutnb.ac.th',
     'depends': ['base', 'aims_student_academic'],
-
-    # always loaded
     'data': [
-        'security/ir.model.access.csv',
-        'views/transcript_view.xml',
-        'menu/transcript_menu.xml',
+        'security/transcript_security.xml',
         'reports/report_menu.xml',
+        'security/ir.model.access.csv',
         'reports/report_transcript.xml',
+        'views/transcript_view.xml',
+        'views/grades_view.xml',
+        'views/student_grades_view.xml',
+        'menu/transcript_menu.xml',
     ],
-    'demo': [],
     'installable': True,
     'application': False,
     'auto_install': False,
 }
+
